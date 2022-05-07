a42d160562cff826f7094367fbf84304c34c368f
Olivier Richard <olivier.richard@imag.fr>
2022-05-07 20:10:28 +0200

    ear-oar: set ulimit -l via PAM and add better OAR nodes' resources declaration

--- ear-oar/composition.nix
+++ ear-oar/composition.nix
@@ -6,19 +6,26 @@
         imports = [ commonConfig ];
         services.oar.node = {
           enable = true;
-          register = { enable = true; };
+          #register = { enable = true; };
         };
+        services.ear.daemon.enable = true;
+        services.ear.db_manager.enable = true;
       };
+      
     in {
       frontend = { ... }: {
         imports = [ commonConfig ];
         services.oar.client.enable = true;
       };
-
       server = { ... }: {
         imports = [ commonConfig ];
         services.oar.server.enable = true;
         services.oar.dbserver.enable = true;
+        services.ear.database.enable = true;
+      };
+      eargm = { ... }: {
+        imports = [ commonConfig ];
+        services.ear.global_manager.enable = true;
       };
     } // helpers.makeMany node "node" 2;
 
