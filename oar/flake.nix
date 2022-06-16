a8c7314cb82ad02816ce95909aa48fdb3920afe1
Millian Poquet <millian.poquet@inria.fr>
2022-06-16 16:01:18 +0200

    oar: working setup without impure hacks

--- oar/flake.nix
+++ oar/flake.nix
@@ -2,29 +2,27 @@
   description = "OAR - basic setup";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/22.05";
     nxc.url = "git+https://gitlab.inria.fr/nixos-compose/nixos-compose.git";
     nxc.inputs.nixpkgs.follows = "nixpkgs";
     NUR.url = "github:nix-community/NUR";
-    kapack.url = "github:oar-team/nur-kapack/oar";
+    kapack.url = "github:oar-team/nur-kapack/master";
     kapack.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nxc, NUR, kapack}:
-    let
-      system = "x86_64-linux";
-    in {
-      packages.${system} = nxc.lib.compose {
-        inherit nixpkgs system NUR;
-        repoOverrides = { inherit kapack; };
-        setup = ./setup.toml;
-        composition = ./composition.nix;
-        };
-      
-      defaultPackage.${system} =
-        self.packages.${system}."composition::nixos-test";
+  outputs = { self, nixpkgs, nxc, NUR, kapack }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = nxc.lib.compose {
+      inherit nixpkgs system NUR;
+      repoOverrides = { inherit kapack; };
+      composition = ./composition.nix;
+    };
 
-      devShell.${system} = nxc.devShells.${system}.nxcShell;
-     };
+    devShell.${system} = nxc.devShells.${system}.nxcShell.overrideAttrs (base: {
+      buildInputs = base.buildInputs ++ [ pkgs.tmux ];
+    });
+  };
 }
-     
