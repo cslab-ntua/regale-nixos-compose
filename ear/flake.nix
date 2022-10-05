db2c1bd09e59b172ab09c6196d1e24bd5e194384
Olivier Richard <olivier.richard@imag.fr>
2022-10-05 18:07:53 +0200

    [ear] set kapack input to regale branch

--- ear/flake.nix
+++ ear/flake.nix
@@ -6,7 +6,7 @@
     nxc.url = "git+https://gitlab.inria.fr/nixos-compose/nixos-compose.git";
     nxc.inputs.nixpkgs.follows = "nixpkgs";
     NUR.url = "github:nix-community/NUR";
-    kapack.url = "github:oar-team/nur-kapack";
+    kapack.url = "github:oar-team/nur-kapack?ref=regale";
     kapack.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -19,11 +19,8 @@
         repoOverrides = { inherit kapack; };
         setup = ./setup.toml;
         composition = ./composition.nix;
-        };
+      };
       
-      defaultPackage.${system} =
-        self.packages.${system}."composition::nixos-test";
-
       devShell.${system} = nxc.devShells.${system}.nxcShell;
     };
 }
