c66123b7bb8cfd369dfa2bdd5fe194b420f07a6f
Olivier Richard <olivier.richard@imag.fr>
2022-10-18 13:48:28 +0200

    [ear-oar] Add Readme and helper script to launch mpirun with EAR's envars, small refactoring

--- ear-oar/common_config.nix
+++ ear-oar/common_config.nix
@@ -2,26 +2,11 @@
 let
   inherit (import "${toString modulesPath}/tests/ssh-keys.nix" pkgs)
     snakeOilPrivateKey snakeOilPublicKey;
-  add_resources = pkgs.writers.writePython3Bin "add_resources" {
-    libraries = [ pkgs.nur.repos.kapack.oar ]; } ''
-    from oar.lib.tools import get_date
-    from oar.lib.resource_handling import resources_creation
-    import sys
-    import time
-    r = True
-    while r:
-        try:
-            print(get_date())  # date took from db (test connection)
-            r = False
-        except Exception:
-            print("DB is not ready")
-            time.sleep(0.25)
-    resources_creation("node", int(sys.argv[1]), int(sys.argv[2]))
-  '';
+  scripts = import ./scripts/scripts.nix { inherit pkgs; };
 in {
   imports = [ nur.repos.kapack.modules.oar nur.repos.kapack.modules.ear ];
   
-  environment.systemPackages = [ pkgs.python3 pkgs.nano pkgs.mariadb pkgs.cpufrequtils pkgs.nur.repos.kapack.npb pkgs.openmpi pkgs.taktuk ];
+  environment.systemPackages = [ pkgs.python3 pkgs.nano pkgs.mariadb pkgs.cpufrequtils pkgs.nur.repos.kapack.npb pkgs.openmpi pkgs.taktuk scripts.oar_ear_mpirun];
   
   environment.variables.EAR_INSTALL_PATH = "${pkgs.nur.repos.kapack.ear}";
   environment.variables.EAR_ETC = "/etc";
@@ -30,10 +15,11 @@ in {
   # Allow root yo use open-mpi
   environment.variables.OMPI_ALLOW_RUN_AS_ROOT = "1";
   environment.variables.OMPI_ALLOW_RUN_AS_ROOT_CONFIRM = "1";
- 
+
   networking.firewall.enable = false;
-  users.users.user1 = { isNormalUser = true; };
-  users.users.user2 = { isNormalUser = true; };
+
+  users.users.user1 = { isNormalUser = true; home = "/users/user1"; };
+  users.users.user2 = { isNormalUser = true; home = "/users/user2"; };
 
   security.pam.loginLimits = [
     { domain = "*"; item = "memlock"; type = "-"; value = "unlimited"; }
@@ -44,11 +30,13 @@ in {
     mode = "0600";
     source = snakeOilPrivateKey;
   };
+
   environment.etc."pubkey.snakeoil" = {
     mode = "0600";
     #source = snakeOilPublicKey;
     text = snakeOilPublicKey;
   };
+
   environment.etc."oar-dbpassword".text = ''
     # DataBase user name
     DB_BASE_LOGIN="oar"
@@ -62,36 +50,17 @@ in {
     # DataBase read only user password
     DB_BASE_PASSWD_RO="oar_ro" 
   '';
-  environment.etc."oar/ear_newjob.sh".source = pkgs.writeShellScript "ear_newjob"
-  ''
-    uniq $OAR_FILE_NODES > "/tmp/uniq_oar_nodes_$OAR_JOB_ID"
-    $EAR_INSTALL_PATH/bin/oar-ejob 50001 newjob "/tmp/uniq_oar_nodes_$OAR_JOB_ID"
-  '';
-  environment.etc."oar/ear_endjob.sh".source = pkgs.writeShellScript "ear_endjob"
-  ''
-    $EAR_INSTALL_PATH/bin/oar-ejob 50001 endjob "/tmp/uniq_oar_nodes_$OAR_JOB_ID"
-    #rm "/tmp/uniq_oar_nodes_$OAR_JOB_ID"
-  '';
+
+  environment.etc."oar/ear_newjob.sh".source = scripts.ear_newjob;
+  environment.etc."oar/ear_endjob.sh".source = scripts.ear_endjob;
 
   services.oar = {
     # oar db passwords
     database = {
       host = "server";
       passwordFile = "/etc/oar-dbpassword";
-      initPath = [ pkgs.util-linux pkgs.gawk pkgs.jq add_resources];
-      postInitCommands = ''
-      num_cores=$(( $(lscpu | awk '/^Socket\(s\)/{ print $2 }') * $(lscpu | awk '/^Core\(s\) per socket/{ print $4 }') ))
-      echo $num_cores > /etc/num_cores
-      
-      if [[ -f /etc/nxc/deployment-hosts ]]; then
-        num_nodes=$(grep node /etc/nxc/deployment-hosts | wc -l)
-      else
-        num_nodes=$(jq -r '[.nodes[] | select(contains("node"))]| length' /etc/nxc/deployment.json)
-      fi
-      echo $num_nodes > /etc/num_nodes
-      
-      add_resources $num_nodes $num_cores 
-      '';
+      initPath = [ pkgs.util-linux pkgs.gawk pkgs.jq scripts.add_resources];
+      postInitCommands = scripts.oar_db_postInitCommands;
     };
     server.host = "server";
     privateKeyFile = "/etc/privkey.snakeoil";
