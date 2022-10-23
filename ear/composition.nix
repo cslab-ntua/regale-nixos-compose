29b1d57ec119c3dcfac0656c6c8d7abc7d6d930d
Olivier Richard <olivier.richard@imag.fr>
2022-10-23 12:07:47 +0200

    [ear] add embedded test-script

--- ear/composition.nix
+++ ear/composition.nix
@@ -29,6 +29,35 @@
     };
   
   testScript = ''
-      eardb.succeed("true")
+  #prepare machine files for mpirun and eat
+  node1.execute('yes node1  | head -n 16 > machines && yes node2  | head -n 16 >> machines')
+  node1.execute('uniq machines > uniq_machines')
+
+  #signal ear daemon for new job to monitor
+  node1.execute('OAR_JOB_ID=1 OAR_USER=user1 oar-ejob 50001 newjob uniq_machines')
+
+  #launch cgi.C.mpi application
+  node1.succeed("""
+  mpirun --hostfile machines --mca btl tcp,self \
+  -x LD_PRELOAD=$EAR_INSTALL_PATH/lib/libearld.so \
+  -x OAR_EAR_LOAD_MPI_VERSION=ompi \
+  -x OAR_STEP_NUM_NODES=2 \
+  -x OAR_JOB_ID=1 \
+  -x OAR_STEP_ID=0 \
+  cg.C.mpi
+  """)
+
+  #signal end of job
+  node1.execute('OAR_JOB_ID=1 OAR_USER=user1 oar-ejob 50001 endjob uniq_machines')
+
+  #wait for monitoring data availability w/ timeout (5s) and save then in file
+  node1.execute("""sec=0; until [ -f result ] || [ $sec -gt 4 ]; \
+  do eacct -c result; \ 
+  sleep 1; \ 
+  sec=$((sec + 1)); \
+  done')
+  
+  #test if monitoring data file exists 
+  node1.succeed('[ -f result ]')
   '';
 }
