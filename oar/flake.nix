8715199f333669558c001b752fb5e556c3f47faf
Alexandre Lithaud <alexandre.ltd71@gmail.com>
2023-06-07 10:37:33 +0200

    22.11 working with old version of oar3

--- oar/flake.nix
+++ oar/flake.nix
@@ -2,11 +2,12 @@
   description = "OAR - basic setup";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
     nxc.url = "git+https://gitlab.inria.fr/nixos-compose/nixos-compose.git";
     nxc.inputs.nixpkgs.follows = "nixpkgs";
     NUR.url = "github:nix-community/NUR";
-    kapack.url = "github:oar-team/nur-kapack";
+    kapack.url = "github:oar-team/nur-kapack"; ## Mettre la branche regale 2211 pour avoir une version a ajour de oar (puis ensuite tout envoyer en 23.05) (en copiant on local)
+    #kapack.url = "github:oar-team/nur-kapack/regale-2211";
     #kapack.url = "github:oar-team/nur-kapack?ref=regale";
     kapack.inputs.nixpkgs.follows = "nixpkgs";
   };
