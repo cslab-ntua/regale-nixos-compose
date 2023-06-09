16b93770b0ee2560a300eaf7754f6792c352e37f
Alexandre Lithaud <alexandre.ltd71@gmail.com>
2023-06-09 10:08:13 +0200

    Update bdpo, bdpo-oar, ear and ear-oar, permission access denied

--- bdpo-oar/flake.nix
+++ bdpo-oar/flake.nix
@@ -2,12 +2,13 @@
   description = "BDPO-OAR";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/22.05";
-    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/22.05";
+    # nixpkgs.url = "github:NixOS/nixpkgs/22.11";
     nxc.url = "git+https://gitlab.inria.fr/nixos-compose/nixos-compose.git";
     nxc.inputs.nixpkgs.follows = "nixpkgs";
     NUR.url = "github:nix-community/NUR";
     kapack.url = "github:oar-team/nur-kapack?ref=regale";
+    # kapack.url = "github:oar-team/nur-kapack?ref=regale-2211";
     kapack.inputs.nixpkgs.follows = "nixpkgs";
   };
 
