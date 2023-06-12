6b9ba87f0d4d5a7bfa316005eeed2b876aab83f0
Alexandre Lithaud <alexandre.ltd71@gmail.com>
2023-06-12 14:49:08 +0200

    Deletion of the old oar composition

--- oar/flake.nix
+++ oar/flake.nix
@@ -6,7 +6,7 @@
     nxc.url = "git+https://gitlab.inria.fr/nixos-compose/nixos-compose.git";
     nxc.inputs.nixpkgs.follows = "nixpkgs";
     NUR.url = "github:nix-community/NUR";
-    #kapack.url = "github:oar-team/nur-kapack/regale-2211";
+    #kapack.url = "github:oar-team/nur-kapack/regale-2211"; # Branch does exist anymore
     kapack.url = "github:oar-team/nur-kapack?ref=regale";
     kapack.inputs.nixpkgs.follows = "nixpkgs";
   };
