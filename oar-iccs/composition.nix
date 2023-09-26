7477cf5f76a64107ef73e5344b18be96584a1524
Nikolaos Triantafyllis <ntriantafyllis@frennes>
2023-09-26 14:46:37 +0200

    Some addons

--- oar-iccs/composition.nix
+++ oar-iccs/composition.nix
@@ -24,6 +24,7 @@
         services.oar.dbserver.enable = true;
       };
     } // helpers.makeMany node "node" nodes_number;
+    rolesDistribution = { nodes = 2; };
 
   testScript = ''
     frontend.succeed("true")
